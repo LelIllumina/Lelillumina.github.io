448564c7a4111665b87c03f99beb5bceb3c325b2
Lel Illumina <dragonbilal938@outlook.com>
2024-05-12 04:13:09 +0500

    update users

--- lastfm/users.js
+++ lastfm/users.js
@@ -1,12 +1,17 @@
 const users = [
-    ["LelIllumina", "lel.nekoweb.org"],
-    ["excelesia", ""],
-    ["dimdendev", "dimden.dev"],
-    ["four-caddit", "fourstar.nekoweb.org"],
-    ["garfieldhirou", "apfel.nekoweb.org"],
-    ["maxpixels", "max.nekoweb.org"],
-    ["CorelVulpix", "s1nez.nekoweb.org"],
-    ["tepiloxtl", "tepiloxtl.net"],
-    ["milkywaydotmoe", "milkyway.moe"],
-    ["pupfae", "gup.nekoweb.org"]
-]
+  ["LelIllumina", "lel.nekoweb.org"],
+  ["tepiloxtl", "tepiloxtl.net"],
+  ["excelesia", ""],
+  ["dimdendev", "dimden.dev"],
+  ["four-caddit", "fourstar.nekoweb.org"],
+  ["garfieldhirou", "apfel.nekoweb.org"],
+  ["maxpixels", "max.nekoweb.org"],
+  ["CorelVulpix", "s1nez.nekoweb.org"],
+  ["milkywaydotmoe", "milkyway.moe"],
+  ["pupfae", "gup.nekoweb.org"],
+  ["flowerz-2005", "flowerz.nekoweb.org"],
+];
+const devs = [
+  ["LelIllumina", "lel.nekoweb.org"],
+  ["tepiloxtl", "tepiloxtl.net"],
+];
