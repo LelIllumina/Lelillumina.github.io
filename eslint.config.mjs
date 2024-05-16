a5bf17a0fa5ee1c5621a2dcf93af3f531bcfab3b
Lel Illumina <dragonbilal938@outlook.com>
2024-05-16 18:34:55 +0500

    switch to pnpm

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "script" },
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    parserOptions: {
+      sourceType: "module",
+    },
+  },
+  pluginJs.configs.recommended,
+];
