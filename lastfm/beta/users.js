e429cff046388045b8dbe6f69ede4365be0f7df0
Lel Illumina <dragonbilal938@outlook.com>
2024-05-17 17:44:44 +0500

    add users back

--- lastfm/beta/users.js
+++ lastfm/beta/users.js
@@ -2,7 +2,16 @@
 const users = [
   ["LelIllumina", "lel.nekoweb.org", true],
   ["tepiloxtl", "tepiloxtl.net", false],
+  ["excelesia", "", false],
+  ["dimdendev", "dimden.dev", false],
+  ["four-caddit", "fourstar.nekoweb.org", false],
+  ["garfieldhirou", "apfel.nekoweb.org", false],
   ["maxpixels", "max.nekoweb.org", false],
+  ["CorelVulpix", "s1nez.nekoweb.org", false],
+  ["milkywaydotmoe", "milkyway.moe", false],
+  ["pupfae", "gup.nekoweb.org", false],
+  ["flowerz-2005", "flowerz.nekoweb.org", false],
+  ["abyssmaze", "eye.nekoweb.org", false],
 ];
 const devs = [
   ["LelIllumina", "lel.nekoweb.org"],
