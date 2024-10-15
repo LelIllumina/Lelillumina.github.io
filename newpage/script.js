fec24bb0449dd393942759b943affd7b22273a72
Lel Illumina <dragonbilal938@outlook.com>
2024-10-15 23:11:10 +0500

    Slight Changes

--- newpage/script.js
+++ newpage/script.js
@@ -1,2 +1,5 @@
+// This will use JavaScript to get your username
 const username = window.location.host.split(".")[0];
-document.getElementById("username").textContent = username + "!";
+document.getElementById("username").textContent = username + "!"; // and then set insert it into the element with the "username" Id
+
+document.title = "This is YOUR site " + username; // and on the title!!
