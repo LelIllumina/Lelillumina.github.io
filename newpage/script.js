e44d6f2afe32ae668990e981975a757e59b20bf1
Lel Illumina <dragonbilal938@outlook.com>
2024-10-16 20:40:14 +0500

    jimjen code

--- newpage/script.js
+++ newpage/script.js
@@ -1,5 +1,5 @@
 // This will use JavaScript to get your username
-const username = window.location.host.split(".")[0];
-document.getElementById("username").textContent = username + "!"; // and then set insert it into the element with the "username" Id
+const username = location.hostname.replace(".nekoweb.org", "");
+document.getElementById("username").textContent = username + "!"; // and then insert it into the element with the "username" Id
 
 document.title = "This is YOUR site " + username; // and on the title!!
