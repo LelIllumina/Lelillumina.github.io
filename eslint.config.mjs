83d498806738b2c71ad2ec7d40d64df9b1d427b5
Lel Illumina <dragonbilal938@outlook.com>
2024-11-24 05:16:13 +0500

    chore: run stylelint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,21 +4,19 @@ import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
   {
-    ignores: ["**/*.min.js"], // Ignore all minified JavaScript files
-
+    ignores: ["dist/**"],
     plugins: {
-      prettier: prettierPlugin, // Register the Prettier plugin
+      prettier: prettierPlugin,
     },
 
     rules: {
-      "no-unused-vars": "warn", // Warn for variables that are declared but not used
-      eqeqeq: "error", // Enforce strict equality `===` and `!==`
-      "no-var": "error", // Disallow `var`, encourage `let` and `const`
-      "prefer-const": "warn", // Suggest `const` if a variable is never reassigned
-      semi: ["error", "always"], // Enforce semicolons at the end of statements
-      quotes: ["error", "double"], // Enforce single quotes for strings
-      // indent: ["error", 2], // Enforce 2-space indentation
-      "no-multiple-empty-lines": ["error", { max: 1 }], // Limit empty lines
+      "no-unused-vars": "warn",
+      eqeqeq: "error",
+      "no-var": "error",
+      "prefer-const": "warn",
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+      "no-multiple-empty-lines": ["error", { max: 1 }],
 
       // Enable Prettier as an ESLint rule
       "prettier/prettier": "error",
