ac0b3e593f2fe0ff9a78f94ade4176fdbe289477
Lel Illumina <dragonbilal938@outlook.com>
2024-12-21 03:41:39 +0500

    MegaNuke

--- cssnano.config.js
+++
@@ -1,4 +0,0 @@
-export const preset = [
-  require("cssnano-preset-advanced"),
-  { autoprefixer: false },
-];
--- public/newpage/script.js
+++
@@ -1,5 +0,0 @@
-// This will use JavaScript to get your username
-const username = location.hostname.replace(".nekoweb.org", "");
-document.getElementById("username").textContent = username + "!"; // and then insert it into the element with the "username" Id
-
-document.title = "This is YOUR site " + username; // and on the title!!
