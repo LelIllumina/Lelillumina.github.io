4de026357541429205a59a8c20bdb466ba9e9262
Lel Illumina <dragonbilal938@outlook.com>
2025-03-22 02:07:42 +0500

    feat: move old blogs to archive

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,7 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
 
-// 2. Import loader(s)
 import { glob } from "astro/loaders";
 
-// 3. Define your collection(s)
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
   schema: z.object({
@@ -13,6 +10,16 @@ const blog = defineCollection({
     date: z.date(),
   }),
 });
+const archive = defineCollection({
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "./src/content/archive",
+  }),
+  schema: z.object({
+    title: z.string(),
+    desc: z.string(),
+    date: z.date(),
+  }),
+});
 
-// 4. Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = { blog, archive };
