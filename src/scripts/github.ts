1cc164f1d4d8cacf4813780f39e3da410cbf3581
Lel Illumina <dragonbilal938@outlook.com>
2025-03-23 03:13:03 +0500

    feat: new last commit widget

---
+++ src/scripts/github.ts
@@ -0,0 +1,22 @@
+fetch(
+  "https://api.github.com/repos/lelillumina/lelillumina.github.io/commits?per_page=1",
+)
+  .then((res) => res.json())
+  .then((res) => {
+    let sha = res[0].sha;
+    let authorDate = new Date(res[0].commit.author.date);
+    (
+      document.getElementById("commitLatest") as HTMLParagraphElement
+    ).innerText = res[0].commit.message;
+    (document.getElementById("shortHash") as HTMLParagraphElement).innerText =
+      "latest commit:" +
+      sha.substring(0, 7) +
+      " on " +
+      authorDate.toLocaleDateString("en-GB", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "2-digit",
+      });
+    (document.getElementById("commitLink") as HTMLAnchorElement).href =
+      "https://github.com/lelillumina/lelillumina.github.io/commit/" + sha;
+  });
